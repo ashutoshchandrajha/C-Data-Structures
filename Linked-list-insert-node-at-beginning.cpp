c1c34b45f9b74a8c61518ee50e3603d22978701e
ashutoshchandrajha <ashutosh.jha@nyu.edu>
2016-01-22 17:45:39 -0500

    Update Linked-list-insert-node-at-beginning.cpp

--- Linked-list-insert-node-at-beginning.cpp
+++ Linked-list-insert-node-at-beginning.cpp
@@ -1,3 +1,5 @@
+////Declaring head as global variable
+
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
@@ -32,5 +34,49 @@ int main(){
     for(int i=0;i<n;i++){
         Insert();
         Print();
+    }
+}
+
+
+//////Passing head in call to function
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+void Insert(int i,struct Node** head)
+{
+    struct Node* temp = (Node*)malloc(sizeof(struct Node));
+    temp->data=i;
+    temp->next=*head;
+    *head = temp;
+}
+void Print(struct Node* head)
+{
+    while(head!=NULL)
+    {
+        printf("%d ",head->data);
+        head=head->next;
+    }
+    printf("\n");
+}
+int main()
+{
+    int i,n,j;
+    struct Node* head = NULL;
+    printf("How many numbers you want to enter? ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("Enter a number = ");
+        scanf("%d",&j);
+        Insert(j,&head);
+        Print(head);
     }
 }
